7d44524dc9148151eaf5587195586accc9c2965b
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-06-09 23:33:02 +0700

    Second commit

--- db_admin.sql
+++ db_admin.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 13, 2020 at 07:00 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Host: localhost:3307
+-- Generation Time: Jun 09, 2024 at 06:25 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_roles` (
   `id` int(11) NOT NULL COMMENT 'role_id',
   `role` varchar(255) DEFAULT NULL COMMENT 'role_text'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_roles`
@@ -59,22 +58,23 @@ CREATE TABLE `tbl_users` (
   `isActive` tinyint(4) DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
 INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
-(7, 'Nababur', 'Nababurbd', 'nababurbd@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 1, 0, '2020-03-12 16:23:01', '2020-03-12 16:23:01'),
-(12, 'Rayhan', 'Rayhan', 'rayhankabir@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-12 18:20:24', '2020-03-12 18:20:24'),
-(15, 'Sanjia Akther', 'Sanjida', 'sanjida@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-12 19:32:27', '2020-03-12 19:32:27'),
-(16, 'Abid Ali', 'Abid', 'abid@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 3, 0, '2020-03-13 05:08:26', '2020-03-13 05:08:26'),
-(17, 'Abdur Rouf', 'Rouf', 'rouf@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:08:53', '2020-03-13 05:08:53'),
-(18, 'Maruf Jaman', 'Maruf', 'maruf@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:09:18', '2020-03-13 05:09:18'),
-(19, 'Humayun ', 'Munna', 'munna@gmail.com', '66c3241204bea40578eb993f41f7c4b1ab982dab', '01717090233', 3, 0, '2020-03-13 05:09:49', '2020-03-13 05:09:49'),
-(20, 'Rased ', 'Rashed', 'rashed@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 1, '2020-03-13 05:10:24', '2020-03-13 05:10:24'),
-(21, 'Millon ', 'Millon', 'millon@gmail.com', '05c19fb114728eabf85f47c858914ca42ddd2dae', '01717090233', 1, 1, '2020-03-13 05:11:02', '2020-03-13 05:11:02');
+(16, 'Jonhny Joestar', 'Jojo', 'jojo@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 3, 0, '2020-03-13 05:08:26', '2020-03-13 05:08:26'),
+(17, 'Lee Sang Hyeok', 'Faker', 'faker@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:08:53', '2020-03-13 05:08:53'),
+(18, 'Chu Anh Quân', 'Rem_sama', 'Remsama@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:09:18', '2020-03-13 05:09:18'),
+(19, 'Nguyễn Minh Quân', 'alivewifi', 'alivewifi@gmail.com', '66c3241204bea40578eb993f41f7c4b1ab982dab', '01717090233', 3, 0, '2020-03-13 05:09:49', '2020-03-13 05:09:49'),
+(20, 'Nguyễn Nhật Minh', 'VeraBl', 'VeraBL@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:10:24', '2020-03-13 05:10:24'),
+(21, 'Nguyễn Hữu Mạnh', 'Silverss03', 'nguyenhuumanhhp2003@gmail.com', '05c19fb114728eabf85f47c858914ca42ddd2dae', '0528538143', 1, 0, '2020-03-13 05:11:02', '2020-03-13 05:11:02'),
+(22, 'Nguyễn Hữu Mạnh', 'newuser', 'elfshumanresource@gmail.com', '14459f84a76df0cc57ffd1bf6a17149bfd09f82b', '0915166497', 1, 0, '2024-05-18 06:13:05', '2024-05-18 06:13:05'),
+(28, 'Admin', 'administrator', 'newadmin@gmail.com', '14459f84a76df0cc57ffd1bf6a17149bfd09f82b', '0528538143', 1, 0, '2024-05-18 07:41:28', '2024-05-18 07:41:28'),
+(29, 'User1', 'User2', 'newuser@gmail.com', '14459f84a76df0cc57ffd1bf6a17149bfd09f82b', '1111111111111', 3, 0, '2024-05-24 14:05:10', '2024-05-24 14:05:10'),
+(30, 'Phạm Tuấn Minh', 'ptm20066666', 'doctorwho@gmail.com', '14459f84a76df0cc57ffd1bf6a17149bfd09f82b', '0528538143', 3, 0, '2024-05-25 06:38:55', '2024-05-25 06:38:55');
 
 --
 -- Indexes for dumped tables
@@ -106,7 +106,7 @@ ALTER TABLE `tbl_roles`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
